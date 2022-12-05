002d58abc55b5f28fdcf8e3a433799b6c96b3842
nhanhdep <77265657+nhanhdep@users.noreply.github.com>
2022-12-06 04:12:18 +0700

    Add files via upload

---
+++ trong-sinh-sau-8-van-nam.js
@@ -0,0 +1,53 @@
+$(function() { 
+tap_3(); 
+server_1(); 
+}); 
+</script> 
+<!-- hhkungfu.org -->
+<div id="xemphim">
+<!-- hhkungfu.org -->
+<div class="video-wrapper"><iframe id="phim" src="" allowfullscreen="" scrolling="yes" title="player"></iframe></div>
+<script>
+document.write(underplayer);
+</script>
+<!-- hhkungfu.org -->
+<!-- hhkungfu.org -->
+<div class="phim1">
+<center>
+<div class="phim2"><span id="TenServer"><span class="main-color">S1</span></span> | <span id="TenTap"></span></div>
+<span id="taptruoc"></span>
+<button class="server" onclick="server_1();changeText1();">S1</button>
+<button class="server" onclick="server_2();changeText2();">S2</button>
+<button class="server" onclick="server_3();changeText3();">S3</button>
+<button class="server" onclick="server_4();changeText4();">S4</button>
+<span id="taptiep"></span>
+</center>
+</div>
+<!-- hhkungfu.org -->
+<!-- hhkungfu.org -->
+<!-- hhkungfu.org -->
+<!-- hhkungfu.org -->
+<!-- hhkungfu.org -->
+<div class="list-tap">
+<button class="tap" onclick="clearfocus();tap_1();server_1();changeText1();click_func(id);" id="1">1</button> 
+<button class="tap" onclick="clearfocus();tap_2();server_1();changeText1();click_func(id);" id="2">2</button> 
+<button class="tap" onclick="clearfocus();tap_3();server_1();changeText1();click_func(id);" id="3">3</button> 
+</div> 
+<!-- hhkungfu.org --> 
+</div> 
+<!-- hhkungfu.org --> 
+<script>
+var link_1_3 = "../../p/playervideo.html?vid=41ae36ecb9b3eee609d05b90c14222fb"
+var link_2_3 = "../../p/iframe.html?vid=http://syntheticvideo.xyz/embed/dc9c7c4f851296b0e783816ad05cb5da"
+var link_3_3 = "https://youtube.com/embed/qwyBRBj2_M8"
+var link_4_3 = "../../p/shortink.html?vid=Y7QGIvM5s"
+
+var link_1_2 = "../../p/shortink.html?vid=78kwJRw1I"
+var link_2_2 = "../../p/iframe.html?vid=http://syntheticvideo.xyz/embed/8945bc7beb8297426193dfce3da1f405"
+var link_3_2 = "../../p/shortink.html?vid=78kwJRw1I"
+var link_4_2 = "../../p/shortink.html?vid=78kwJRw1I"
+
+var link_1_1 = "../../p/shortink.html?vid=7VnXONrDb5"
+var link_2_1 = "../../p/iframe.html?vid=http://syntheticvideo.xyz/embed/d4c999d11f43cacb0a2f54daba247c66"
+var link_3_1 = "../../p/shortink.html?vid=7VnXONrDb5"
+var link_4_1 = "../../p/shortink.html?vid=7VnXONrDb5"
